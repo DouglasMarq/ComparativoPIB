14a6d7b25159929c481d92920a8aad2be11e821d
Douglas Marques Alves <Douglas.marq.alves@gmail.com>
2018-04-22 20:14:16 -0300

    Implementação do leitor de Arquivo CSV

--- src/Classes/Main_Class.java
+++ src/Classes/Main_Class.java
@@ -7,11 +7,13 @@ public class Main_Class {
 
     public static void main(String[] args) {
                
+        //instancia e chama a classe para ler o arquivo -- utilizado para testes
         Ler_Arquivo lerCSV = new Ler_Arquivo();
         lerCSV.arquivocsv();
         
-        //Main_Window JanelaPrincipal = new Main_Window();
-        //JanelaPrincipal.setVisible(true);
+        //Cria a janela Main_Window
+        Main_Window JanelaPrincipal = new Main_Window();
+        JanelaPrincipal.setVisible(true);
         
     }
     
