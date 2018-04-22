c2ef9c9299f6cf3f215942eb486b7facf4645797
Douglas Marques Alves <Douglas.marq.alves@gmail.com>
2018-04-22 19:29:55 -0300

    Initial commit

---
+++ src/Classes/Main_Class.java
@@ -0,0 +1,15 @@
+package Classes;
+
+import UI.Main_Window;
+
+public class Main_Class {
+
+
+    public static void main(String[] args) {
+               
+        Main_Window JanelaPrincipal = new Main_Window();
+        JanelaPrincipal.setVisible(true);
+        
+    }
+    
+}
